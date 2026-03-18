6315cd99037d81b8e0ed385fbeddece51e553d65
Loganbh0 <loganbrianhanson@gmail.com>
2026-03-18 13:18:19 -0600

    New repository with .gitignore files

--- backend/Marginalia.API/obj/Debug/net10.0/Marginalia.API.AssemblyInfo.cs
+++ backend/Marginalia.API/obj/Debug/net10.0/Marginalia.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Marginalia.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e8b576bafa1e0623aa448f5de9967527b67f03c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99ab92dbd90971351110c5cc61d5ce2952e9e443")]
 [assembly: System.Reflection.AssemblyProductAttribute("Marginalia.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Marginalia.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
